e9b5b38d704cc3328f01d6df097896644275eabc
madhavi-mishra <64478605+madhavi-mishra@users.noreply.github.com>
2021-08-13 23:14:13 +0530

    week1.day2.string

---
+++ ChangeOddIndexToUpperCase.java
@@ -0,0 +1,39 @@
+package week1day2.Strings;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test = "changeme";
+		char[] characters = test.toCharArray();
+		System.out.println("Changed odd index to upper case:");
+		for (int i = 0; i < characters.length; i++) {
+			char c = characters[i];
+			if (i % 2 != 0) {
+				c = Character.toUpperCase(c);
+
+			}
+				System.out.print( c );
+
+		}
+	}
+
+}
+
+/*
+ * Pseudo Code
+ * 
+ * Declare String Input as Follow
+ * 
+ * String test = "changeme";
+ * 
+ * a) Convert the String to character array
+ * 
+ * b) Traverse through each character (using loop)
+ * 
+ * c)find the odd index within the loop (use mod operator)
+ * 
+ * d)within the loop, change the character to uppercase, if the index is odd
+ * else don't change
+ * 
+ */
