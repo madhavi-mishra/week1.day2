23ea600ed16cefaf4e4ba9ee5d4fe3fad2334fe9
madhavi-mishra <64478605+madhavi-mishra@users.noreply.github.com>
2021-08-18 18:47:35 +0530

    Update ReverseEvenWords.java

--- ReverseEvenWords.java
+++ ReverseEvenWords.java
@@ -4,7 +4,19 @@ public class ReverseEvenWords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Unable tp crack the logic
+		String test = "I am a software tester";
+		String[] t = test.split(" ");
+
+		String val = "";
+		for (int i = 0; i < t.length; i++) {
+
+			if (i % 2 == 1)
+				val = val + new StringBuilder(t[i]).reverse().toString() + " ";
+			else
+				val = val + t[i] + " ";
+		}
+		val = val.trim();
+		System.out.println(val);
 
 	}
 
